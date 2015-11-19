800e59ce052ac3de059eb06876ff791ea7aed633
Michael <michael@nsideas.com>
2015-11-19 14:00:40 -0500

    went back to object

--- twig/decodeJSONTwigExtension.php
+++ twig/decodeJSONTwigExtension.php
@@ -25,17 +25,10 @@ class decodeJSONTwigExtension extends \Twig_Extension
     }
 
     public function decodeJSON($file){
-        $response = array();
         
         $content = file_get_contents($file, true);
         
-        $stdClassObject = json_decode($content);
-        
-        foreach ($stdClassObject as $key => $value){
-            $response[]= array($key, $value);
-        }
-
-        return $response;
+        return json_decode($content);
 
     }
 
